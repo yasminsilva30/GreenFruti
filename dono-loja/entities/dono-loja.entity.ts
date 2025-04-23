1c2214212c645ec9c9ec4517efb0a4b9b827eb7e
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-23 04:06:43 -0300

    Add files via upload

---
+++ dono-loja/entities/dono-loja.entity.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { Expose } from 'class-transformer';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Loja } from 'src/loja/entities/loja.entity';
+
+@Entity()
+export class DonoLoja {
+  @PrimaryGeneratedColumn()
+  @Expose()
+  id: number;
+
+  @Column()
+  @Expose()
+  nome: string;
+
+  @Column()
+  @Expose()
+  cpf: string;
+
+  @ManyToOne(() => Loja, (loja) => loja.donosLoja)
+  loja: Loja;
+}
---
+++ loja/dto/create-loja.dto.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { IsString, IsOptional, IsNumber } from 'class-validator';
+
+export class CreateLojaDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  endereco: string;
+
+  @IsOptional()
+  @IsNumber()
+  latitude?: number;
+
+  @IsOptional()
+  @IsNumber()
+  longitude?: number;
+}
