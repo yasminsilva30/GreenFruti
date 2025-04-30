627b5669ed99971d94153a48fcfcbd0eac1c43fb
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-30 10:39:15 -0300

    Delete loja directory

--- loja/dto/update-loja.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLojaDto } from './create-loja.dto';
-
-export class UpdateLojaDto extends PartialType(CreateLojaDto) {}
