fb4e183b3a2614dad0373d7faa261aa954cd95e4
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-30 10:39:08 -0300

    Delete dono-loja directory

--- dono-loja/dto/update-dono-loja.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDonoLojaDto } from './create-dono-loja.dto';
-
-export class UpdateDonoLojaDto extends PartialType(CreateDonoLojaDto) {}
