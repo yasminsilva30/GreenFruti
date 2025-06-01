c1517d002054a0371c16eb0de1f28f286db4e043
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-06-01 17:52:19 -0300

    Add files via upload

---
+++ greenfruti/src/dono-loja/dto/create-dono-loja.dto.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDonoLojaDto {
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  telefone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  senha: string;
+}
+
