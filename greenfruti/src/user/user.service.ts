c4cf8118c771c2e823539c03d653e17a4e56ecfe
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-06-09 19:33:45 -0300

    Update user.service.ts

--- greenfruti/src/user/user.service.ts
+++ greenfruti/src/user/user.service.ts
@@ -1,50 +1,35 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
-import { UserRole } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private userRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create({
-      ...createUserDto,
-      role: UserRole.ADMIN,  // DEFINA AQUI O PAPEL QUE DESEJA UTILIZAR
-    });    
-  
-    const savedUser = await this.userRepository.save(user);
-  
-    const { senha, ...userWithoutPassword } = savedUser;
-    return userWithoutPassword;
-  }
-
-  async findAll() {
-    return this.userRepository.find();
-  }
-
-  async findOne(id: number) {
-    const user = await this.userRepository.findOne({ where: { id } });
-    if (!user) {
-      throw new NotFoundException('Usuário não encontrado...');
-    }
-    return user;
-  }
-
-  async update(id: number, updateUserDto: UpdateUserDto) {
-    const user = await this.findOne(id);
-    Object.assign(user, updateUserDto);
+  create(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create(createUserDto);
     return this.userRepository.save(user);
   }
 
-  async remove(id: number) {
-    const user = await this.findOne(id);
-    return this.userRepository.remove(user);
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
   }
 }
