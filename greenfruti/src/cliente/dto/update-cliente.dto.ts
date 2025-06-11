02585050012fe648761575237a4b8e3a48e4482b
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-06-10 23:14:56 -0300

    Delete greenfruti/src/cliente directory

--- greenfruti/src/cliente/dto/update-cliente.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClienteDto } from './create-cliente.dto';
-
-export class UpdateClienteDto extends PartialType(CreateClienteDto) {}
