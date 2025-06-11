d0fb5028477b3a902af5b3c88b5c29952e6572f1
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-06-10 23:15:24 -0300

    Delete greenfruti/src/motoboy directory

--- greenfruti/src/motoboy/dto/update-motoboy.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMotoboyDto } from './create-motoboy.dto';
-
-export class UpdateMotoboyDto extends PartialType(CreateMotoboyDto) {}
